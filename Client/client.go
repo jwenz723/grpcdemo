494af25c81ddf96d6ccc780c0def0d1a9b8833db
jwenz723 <jeff.wenzbauer@incontact.com>
2019-03-17 01:15:46 -0600

    Added prometheus metrics. Tuning of kubernetes deployment.

--- Client/client.go
+++ Client/client.go
@@ -4,14 +4,22 @@ import (
 	"context"
 	"flag"
 	pb "github.com/jwenz723/grpcdemo/notestream"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
 var (
-	serverAddrFlag = flag.String("server_addr", "", "The server address in the format of host:port")
+	serverAddrFlag   = flag.String("server_addr", "", "The server address in the format of host:port")
+	grpcMessagesSent = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "grpc_messages_sent",
+		Help: "The total number of grpc messages sent",
+	})
 )
 
 func main() {
@@ -24,6 +32,12 @@ func main() {
 	} else if serverAddrEnv, ok := os.LookupEnv("SERVER_ADDR"); ok {
 		serverAddr = serverAddrEnv
 	}
+
+	// Start prometheus metrics server
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2112", nil)
+	}()
 
 	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
 	if err != nil {
@@ -39,9 +53,10 @@ func main() {
 	defer stream.CloseSend()
 
 	waitc := make(chan struct{})
+	n := &pb.Note{Sender: "client", Message: "A message from a client"}
 	go func() {
-		for i := 0; i < 5; i++ {
-			in, err := stream.Recv()
+		for {
+			_, err := stream.Recv()
 			if err == io.EOF {
 				// read done.
 				close(waitc)
@@ -51,10 +66,11 @@ func main() {
 				log.Fatalf("Failed to receive a note : %v", err)
 			}
 
-			log.Printf("%s: %s\n", in.Sender, in.Message)
-			if err := stream.Send(&pb.Note{Sender: "client", Message: "A message from a client"}); err != nil {
+			//log.Printf("%s: %s\n", in.Sender, in.Message)
+			if err := stream.Send(n); err != nil {
 				log.Fatalf("Failed to send a note: %v", err)
 			}
+			grpcMessagesSent.Inc()
 		}
 	}()
 
--- Server/server.go
+++ Server/server.go
@@ -4,10 +4,12 @@ import (
 	"flag"
 	"fmt"
 	pb "github.com/jwenz723/grpcdemo/notestream"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 	"io"
 	"log"
 	"net"
+	"net/http"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -15,8 +17,9 @@ type server struct{}
 
 // StreamResponse implements helloworld.GreeterServer
 func (s *server) StreamNotes(stream pb.NoteStream_StreamNotesServer) error {
+	n := &pb.Note{Sender: "server", Message: "A message from the server"}
 	for {
-		in, err := stream.Recv()
+		_, err := stream.Recv()
 		if err == io.EOF {
 			return nil
 		}
@@ -25,8 +28,8 @@ func (s *server) StreamNotes(stream pb.NoteStream_StreamNotesServer) error {
 			return err
 		}
 
-		log.Printf("%s: %s\n", in.Sender, in.Message)
-		if err := stream.Send(&pb.Note{Sender: "server", Message: "A message from the server"}); err != nil {
+		//log.Printf("%s: %s\n", in.Sender, in.Message)
+		if err := stream.Send(n); err != nil {
 			return err
 		}
 	}
@@ -43,6 +46,12 @@ var (
 
 func main() {
 	flag.Parse()
+
+	// Start prometheus metrics server
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2111", nil)
+	}()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
