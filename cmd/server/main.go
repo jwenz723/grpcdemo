757b33dcd2cd1a18cfe05c9f3ef0d341fa3fef7e
Jeff Wenzbauer <jwenz723@gmail.com>
2022-07-29 01:15:51 -0600

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -73,9 +73,6 @@ func init() {
 func main() {
 	flag.Parse()
 
-        unusedvar:= "123"
-        unusedvar = "456"
-
         if superfluous() {
                 fmt.Println("something")
         }
